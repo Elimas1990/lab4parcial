90fa7584473e7794e79325c6a3180060ed2de7f9
Elimas1990 <estebanlimas@gmail.com>
2020-10-01 20:58:02 -0300

    practica parcial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   urlPaises:'https://restcountries.eu/rest/v2/',
-  urlPeliculas:'https://my.api.mockaroo.com/peliculas.json?key=001039f0'
+  urlPeliculas:'https://my.api.mockaroo.com/peliculas.json?key=001039f0',
+  urlActores:'https://my.api.mockaroo.com/actores.json?key=001039f0'
 };
 
 /*
